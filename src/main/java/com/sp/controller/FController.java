a3ffee9881a613d7a7e05bc0439aa59c10167067
skadi <skadi660@163.com>
2023-06-12 08:48:57 +0800

    First commit

---
+++ src/main/java/com/sp/controller/FController.java
@@ -0,0 +1,8 @@
+package com.sp.controller;
+
+public class FController {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+    }
+}
